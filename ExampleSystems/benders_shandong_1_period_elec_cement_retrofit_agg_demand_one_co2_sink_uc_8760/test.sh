03660dc9d36f8484a4af74881dd96efc4985c725
anna-x-li <li.anna6389@gmail.com>
2025-12-17 11:14:47 -0500

    add benders examples

--- ExampleSystems/benders_shandong_1_period_elec_cement_retrofit_agg_demand_one_co2_sink_uc_8760/test.sh
+++ ExampleSystems/benders_shandong_1_period_elec_cement_retrofit_agg_demand_one_co2_sink_uc_8760/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=china_31_provinces_cement_elec_8760
+#SBATCH --job-name=china_31_provinces_8760
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=1000G
 #SBATCH --output=slurm-%j.out
-#SBATCH --time=72:00:00
+#SBATCH --time=1:00:00
 #SBATCH --constraint=amd
 #SBATCH --mail-type=all
 #SBATCH --mail-user=al3792@princeton.edu
@@ -18,5 +18,5 @@ module load julia/1.12.1
 # Move to MacroEnergy directory
 cd /home/al3792/MacroEnergy.jl
 
-# Run the case
-julia --project=. "ExampleSystems/china_provinces_1_period_elec_cement_retrofit_agg_demand_one_co2_sink_uc_8760/run.jl"
+# Run case
+julia --project=. ExampleSystems/shandong_1_period_elec_cement_retrofit_agg_demand_one_co2_sink_uc_8760/run.jl
