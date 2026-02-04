668631cd4a0f74a7d534e44699900fcbe2e412ef
Ruaridh Macdonald <rmacd@mit.edu>
2026-02-04 12:46:55 -0500

    Update
    We're relying on pre-processing catching negative discount rates and non-positive period lengths

--- src/utilities/economics.jl
+++ src/utilities/economics.jl
@@ -1,4 +1,4 @@
-function total_years(period_lengths::Vector{Int})
+@inline function total_years(period_lengths::Vector{Int})
     return sum(period_lengths; init=0)
 end
 
@@ -15,8 +15,10 @@ function period_start_years(period_lengths::Vector{Int})
     return result
 end
 
-function present_value_factor(discount_rate::Float64, total_years::Int)
-    # Using a different name than discount factor to avoid overwriting
+@inline function present_value_factor(discount_rate::Float64, total_years::Int)
+    if discount_rate == 0.0
+        return 1.0
+    end
     return 1 / ( (1 + discount_rate) ^ total_years)
 end
 
@@ -24,11 +26,15 @@ function present_value_factor(discount_rate::Float64, period_lengths::Vector{Int
     return present_value_factor.(discount_rate, period_start_years(period_lengths))
 end
 
-function capital_recovery_factor(discount_rate::Float64, total_years::Int)
-    return discount_rate / (1 - (1 + discount_rate) ^ (-total_years))
+@inline function capital_recovery_factor(discount_rate::Float64, total_years::Int)
+    # This presumes we check that total_years is non-negative beforehand
+    if discount_rate == 0.0
+        return 1.0 / total_years
+    end
+    return return discount_rate / (1 - (1 + discount_rate) ^ (-total_years))
 end
 
-function opex_multiplier(discount_rate::Float64, total_years::Int)
+@inline function opex_multiplier(discount_rate::Float64, total_years::Int)
     # sum(1 / (1 + discount_rate)^i) for i 1:N = (1 - (1 + discount_rate)^-N) / discount_rate = 1 / CRF
     return 1 / capital_recovery_factor(discount_rate, total_years)
 end
