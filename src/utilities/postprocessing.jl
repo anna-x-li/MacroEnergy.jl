7aa4c2c2f57971b0c2e7e25de0400c3bd1d83894
Ruaridh Macdonald <rmacd@mit.edu>
2026-03-20 14:20:11 -0400

    postprocess!(Nodes, Model) to calculate effective price of exogenous commodities

--- src/utilities/postprocessing.jl
+++ src/utilities/postprocessing.jl
@@ -6,10 +6,14 @@ function postprocess!(case::Case, solution::Model)
 end
 
 function postprocess!(system::System, solution::Model)
-    # ToDo: we may want to add Nodes, Locations, etc. here too
     for asset in system.assets
         postprocess!(asset, solution)
     end
+
+    for location in system.locations
+        postprocess!(location, solution)
+    end
+
     return nothing
 end
 
@@ -22,6 +26,32 @@ function postprocess!(asset::AbstractAsset, solution::Model)
     end
 
     postprocess_asset!(asset, solution)
+    return nothing
+end
+
+function postprocess!(n::Node, solution::Model)
+    time_steps = collect(time_interval(n))
+    effective_price = zeros(Float64, isempty(time_steps) ? 0 : maximum(time_steps))
+
+    if all(iszero, max_supply(n)) || isempty(supply_flow(n))
+        n.price = effective_price
+        return nothing
+    end
+
+    for t in time_steps
+        total_supply = 0.0
+        total_cost = 0.0
+
+        for s in supply_segments(n)
+            supplied = value(supply_flow(n, s, t))
+            total_supply += supplied
+            total_cost += price_supply(n, s) * supplied
+        end
+
+        effective_price[t] = iszero(total_supply) ? 0.0 : total_cost / total_supply
+    end
+
+    n.price = effective_price
     return nothing
 end
 
